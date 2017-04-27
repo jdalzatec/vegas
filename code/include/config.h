434fa63deb997a724613413c812ead81bee6a948
jdalzatec <jdalzatec@unal.edu.co>
2017-04-27 18:35:06 -0500

    Uploading windows and linux 64 bits versions

---
+++ code/include/config.h
@@ -0,0 +1,7 @@
+#include "/usr/include/hdf5/serial/hdf5.h"
+#include "/usr/include/jsoncpp/json/json.h"
+
+
+// FOR WINDOWS USING CYGWIN
+// #include "/usr/HDF5/1.10.0/include/hdf5.h"
+// #include "/usr/local/include/json/json.h"
